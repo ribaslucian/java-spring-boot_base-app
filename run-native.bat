49a026f0779f7b571b38b458e3cfaab0e201b4bc
Lucian Rossoni Ribas <ribas.lucian@gmail.com>
2022-09-10 19:22:20 -0300

    Creating layout fragments.

--- run-native.bat
+++ run-native.bat
@@ -2,7 +2,8 @@ ECHO OFF
 
 docker run -it --rm ^
     --name my-maven-project ^
-    -v "$(pwd)":/usr/src/mymaven ^
+    --mount type=bind,source=C:\projects\spring-boots\Spring-Base-Application,target=/usr/src/mymaven ^
     -w /usr/src/mymaven ^
-    maven:3.3-jdk-8 ^
-    mvn clean install
+    -p 80:80 ^
+    maven:3.8.6-openjdk-11-slim ^
+    mvn spring-boot:run
--- src/main/java/br/com/baseapp/BaseApplication.java
+++ src/main/java/br/com/baseapp/BaseApplication.java
@@ -2,22 +2,19 @@ package br.com.baseapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+// @RestController
 public class BaseApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(BaseApplication.class, args);
   }
 
-  @GetMapping("/hello")
-  public String hello(
-    @RequestParam(value = "name", defaultValue = "World") String name
-  ) {
-    return String.format("Hello %s!", name);
-  }
+  // @GetMapping("/hello")
+  // public String hello(
+  //   @RequestParam(value = "name", defaultValue = "World") String name
+  // ) {
+  //   return String.format("Hello %s!", name);
+  // }
 }
