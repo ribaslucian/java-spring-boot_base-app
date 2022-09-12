f22fe766b9d8abd7c079278a33b11f36e0eb98ce
Lucian Rossoni Ribas <ribas.lucian@gmail.com>
2022-09-12 18:33:14 -0300

    Auto up by Lucian's script

--- run-native.bat
+++
@@ -1,9 +0,0 @@
-ECHO OFF
-
-docker run -it --rm ^
-    --name my-maven-project ^
-    --mount type=bind,source=C:\projects\spring-boots\Spring-Base-Application,target=/usr/src/mymaven ^
-    -w /usr/src/mymaven ^
-    -p 80:80 ^
-    maven:3.8.6-openjdk-11-slim ^
-    mvn spring-boot:run
